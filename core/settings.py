8123a88dc7e794dc7a32734a2b2f7eb92b36fd63
saken rai <saken.rai.34@gmail.com>
2022-04-16 17:21:27 +0545

    postgres added

--- core/settings.py
+++ core/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
