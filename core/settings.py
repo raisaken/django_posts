806be8f63d5a5538620e564b62752b05b6c6f3ad
saken rai <saken.rai.34@gmail.com>
2022-04-16 13:27:03 +0545

    cors origin added

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://react-posts-tau.vercel.app/",
 ]
 
 
