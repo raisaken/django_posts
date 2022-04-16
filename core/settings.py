7c752a5b23b3d6efcd7eab51ffcef6e4cb779b47
saken rai <saken.rai.34@gmail.com>
2022-04-16 18:58:49 +0545

    vecel cors

--- core/settings.py
+++ core/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://react-posts-tau.vercel",
+    "https://react-posts-tau.vercel.app",
 ]
 
 
